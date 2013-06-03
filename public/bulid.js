8790e9b8b4d109acbb2f312b30f15002e1ad05d5
owner <owner@MS-Workstation>
2013-06-04 01:13:30 +0300

    i18n build support

--- public/bulid.js
+++ public/bulid.js
@@ -1,9 +1,9 @@
 ({
     baseUrl: './',
-	locale: "en-us",
+	locale: "he-il",
 	optimize: "uglify2",
     paths: {
-		i18n: 'static/js/i18n',
+		i18n: 'libs/require/i18n',
 		jquery: 'http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min'
     },
     name: 'core/index',
