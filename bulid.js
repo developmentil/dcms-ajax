09e25e97aed174940fcf88955219133f61c11ccd
owner <owner@MS-Workstation>
2013-06-11 00:55:00 +0300

    remove public folder from git

---
+++ bulid.js
@@ -0,0 +1,12 @@
+({
+    baseUrl: './',
+//	locale: "he-IL",
+	optimize: "uglify2",
+    paths: {
+		i18n: 'libs/require/i18n',
+		jquery: 'http://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min',
+		'dcms-ajax': 'core/index'
+    },
+    name: 'dcms-ajax',
+    out: 'bin/dcms-ajax.js'
+})
